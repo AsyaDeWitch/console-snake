3892c4f0ed5aacbaa44613d8d87876744f8c56ce
AsyaDeWich <asyadenisevich2001@gmail.com>
2020-09-06 22:58:19 +0300

    Добавление класса HorizontalLine

---
+++ Snake/HorizontalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char symb)
+        {
+            pList = new List<Point>();
+            for(int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+        }
+        
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
