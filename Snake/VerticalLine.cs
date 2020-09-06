28cf09631e5007ed0e41a2b2894ee0c59085acbe
AsyaDeWich <asyadenisevich2001@gmail.com>
2020-09-06 23:02:38 +0300

    Добавление класса VerticalLine

---
+++ Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yUp, int yDown, int x, char symb)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
+        }
+}
